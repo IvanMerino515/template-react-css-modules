b10928e3d4af8c75de75d1013541eddb504595e2
IvanMerino515 <merdix515@gmail.com>
2024-12-09 13:52:57 +0100

    updt

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import  Button from './components/button/button';
+
+const App = () => {
+	return <>
+	<Button text = 'Click 1'/>
+	<Button text = 'Click 2'/>
+	<Button text = 'Click 3'/>
+	</>
+};
+
+export default App
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+const Button = (props) => {
+    console.log(props)
+    return <button>{props.text} </button>
+
+}
+
+export default Button
